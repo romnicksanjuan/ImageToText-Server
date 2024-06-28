8d45afc65f77ae7ca6c1fac138618e8267238eba
romnick <romnicksanjuan9@gmail.com>
2024-06-28 22:30:20 +0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+// const { createWorker } = require('tesseract.js');
+const multer = require('multer');
+const Tesseract = require('tesseract.js')
+
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+
+app.post('/', upload.single('image'), (req, res) => {
+
+    try {
+        const imageBuffer = req.file.buffer;
+        Tesseract.recognize(
+            // './image/image.jpg',
+            imageBuffer,
+            'eng',
+            {
+              logger: m => console.log(m)
+            }
+          ).then(({ data: { text } }) => {
+            console.log(text);
+            res.json(text)
+          }).catch(err => {
+            console.error(err);
+          });
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+app.listen(3000, () => {
+    console.log('server is running')
+})   
