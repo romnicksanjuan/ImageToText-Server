1bb021c9420f69e19307387a8dc264e5dc377227
romnick <romnicksanjuan9@gmail.com>
2024-06-29 11:40:23 +0800

    3

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const express = require('express')
 const multer = require('multer');
 const Tesseract = require('tesseract.js')
 
-
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
@@ -17,31 +16,37 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 app.post('/', upload.single('image'), (req, res) => {
 
-    try {
-        const imageBuffer = req.file.buffer;
-        Tesseract.recognize(
-            // './image/image.jpg',
-            imageBuffer,
-            'eng',
-            {
-              logger: m => console.log(m)
-            }
-          ).then(({ data: { text } }) => {
-            console.log(text);
-            res.json(text)
-          }).catch(err => {
-            console.error(err);
-          });
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const imageBuffer = req.file.buffer;
+    // Tesseract.recognize(
+    //     imageBuffer,
+    //     'eng',
+    //     {
+    //       logger: m => console.log(m)
+    //     }
+    //   ).then(({ data: { text } }) => {
+    //     console.log(text);
+    //     res.json(text)
+    //   }).catch(err => {
+    //     console.error(err);
+    //   });
+
+
+    Tesseract.recognize(imageBuffer)
+      .then((result) => {
+        console.log(result.data.text);
+        res.json(result.data.text);
+      });
+  } catch (error) {
+    console.log(error)
+  }
 })
 
-app.get('/potaka', (req,res) =>{
+app.get('/potaka', (req, res) => {
   res.json('potang ina mo')
 })
 
 
 app.listen(3000, () => {
-    console.log('server is running')
+  console.log('server is running')
 })   
